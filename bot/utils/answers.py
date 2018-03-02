8f9f7088913c822a799d78d78c37e4f515d255c7
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-03-02 22:34:15 +0200

    added some new types of answers

--- bot/utils/answers.py
+++ bot/utils/answers.py
@@ -1,5 +1,5 @@
 
-GREETINGS = ('hello', 'hi', 'greetings', '/start')
+GREETINGS = ('hello', 'hi', 'greetings', 'welcome', '/start')
 
 SORRY_ANSWER = 'Sorry, I was created only for greetings.\nSay '
 
@@ -7,6 +7,36 @@ for i in range(len(GREETINGS) - 1):
 	SORRY_ANSWER += '"' + GREETINGS[i] + '", '
 SORRY_ANSWER += 'or "' + GREETINGS[-1] + '".'
 
-RANDOM_ANSWERS = ('LOL', 'KEK', 'ROFL', SORRY_ANSWER)
+RANDOM_ANSWERS = [
+	'LOL',
+	'KEK',
+	'ROFL',
+	'I don\'t understand you.',
+	SORRY_ANSWER
+]
 
-INVALID_MESSAGE_ANSWER = ('Sorry, I can parse only text messages.', 'Are you kidding ?')
+INVALID_MESSAGE_ANSWER = (
+	'Sorry, I can parse only text messages.',
+	'Are you kidding ?',
+	'-_-'
+)
+
+POSITIVE_ANSWERS = [
+	"Yes",
+	'Hmm, okay.',
+	"Of course",
+	"All right",
+	"No doubts"
+]
+
+NEGATIVE_ANSWERS = [
+	"No",
+	"It is forbidden",
+	"I do not allow",
+	"Hope you`re kidding"
+]
+
+NEUTRAL_ANSWERS = [
+	"I do not now yet",
+	"I think yes"
+]
